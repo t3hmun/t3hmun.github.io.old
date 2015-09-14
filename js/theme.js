6811472ab7ee2307a94ed58497a112cb4fe0f583
t3hmun <t3hmun@gmail.com>
2015-09-14 01:02:43 +0100

    Redone the theme switcher.

---
+++ js/theme.js
@@ -0,0 +1,49 @@
+
+var THEMETOGGLE = true;
+var LIGHTTHEME = "/css/main.css"
+var DARKTHEME = "/css/main-light.css"
+
+function toggleTheme()
+{
+    if(THEMETOGGLE){
+        THEMETOGGLE=false;
+        changeTheme(LIGHTTHEME);
+    }else{
+        THEMETOGGLE=true;
+        changeTheme(DARKTHEME);
+    }
+}
+
+function changeTheme(name) {
+    document.getElementById('maincss').setAttribute('href', name);
+}
+
+function changeParaFont(font) {
+    var eles = document.getElementsByTagName('p');
+    for (var i = 0; i < eles.length; i++) {
+        eles[i].style.fontFamily = font;
+        eles[i].style.letterSpacing = 0;
+    }
+}
+
+function customFontPrompt() {
+    font = prompt("Insert custom fontname.");
+    changeParaFont(font);
+}
+
+var FONTTOGGLE = true;
+var DEFAULTFONT = 'Source Sans Pro';
+var OTHERFONT = 'verdana';
+
+function toggleParaFont() {
+
+    if (FONTTOGGLE) {
+        FONTTOGGLE = false;
+        document.getElementById('toggleParaFontButton').innerHTML = DEFAULTFONT;
+        changeParaFont(OTHERFONT);
+    } else {
+        FONTTOGGLE = true;
+        document.getElementById('toggleParaFontButton').innerHTML = OTHERFONT;
+        changeParaFont(DEFAULTFONT);
+    }
+}
