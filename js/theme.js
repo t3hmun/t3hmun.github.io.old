1d34a259444fa6e3cefe4ab4afa83a0b6b54cd89
mun <t3hmun@gmail.com>
2015-10-19 14:32:26 +0100

    basic theme switcher code added.

--- js/theme.js
+++ js/theme.js
@@ -1,34 +1,51 @@
 
 var THEMETOGGLE = true;
-var LIGHTTHEME = "/css/main.css"
-var DARKTHEME = "/css/main-light.css"
+var LIGHTTHEME = "/css/main-light.css";
+var DARKTHEME = "/css/main.css";
 
-function toggleTheme()
-{
-    if(THEMETOGGLE){
-        THEMETOGGLE=false;
-        changeTheme(LIGHTTHEME);
-    }else{
-        THEMETOGGLE=true;
-        changeTheme(DARKTHEME);
-    }
+window.onload = function () {
+  var scriptbuttons = document.getElementById('scripties');
+  scriptbuttons.style.display = 'block';
+  
+  var lighttext = document.getElementById('lightbutton');
+  var darktext = document.getElementById('darkbutton');
+  
+  lighttext.onclick = function(){
+    changeTheme(LIGHTTHEME);
+    return false;
+  };
+  
+  darktext.onclick = function(){
+    changeTheme(DARKTHEME);
+    return false;
+  };
+};
+
+function toggleTheme() {
+  if (THEMETOGGLE) {
+    THEMETOGGLE = false;
+    changeTheme(LIGHTTHEME);
+  } else {
+    THEMETOGGLE = true;
+    changeTheme(DARKTHEME);
+  }
 }
 
 function changeTheme(name) {
-    document.getElementById('maincss').setAttribute('href', name);
+  document.getElementById('maincss').setAttribute('href', name);
 }
 
 function changeParaFont(font) {
-    var eles = document.getElementsByTagName('p');
-    for (var i = 0; i < eles.length; i++) {
-        eles[i].style.fontFamily = font;
-        eles[i].style.letterSpacing = 'normal';
-    }
+  var eles = document.getElementsByTagName('p');
+  for (var i = 0; i < eles.length; i++) {
+    eles[i].style.fontFamily = font;
+    eles[i].style.letterSpacing = 'normal';
+  }
 }
 
 function customFontPrompt() {
-    var font = prompt("Insert custom fontname.");
-    changeParaFont(font);
+  var font = prompt("Insert custom fontname.");
+  changeParaFont(font);
 }
 
 var FONTTOGGLE = true;
@@ -37,13 +54,13 @@ var OTHERFONT = 'verdana';
 
 function toggleParaFont() {
 
-    if (FONTTOGGLE) {
-        FONTTOGGLE = false;
-        document.getElementById('toggleParaFontButton').innerHTML = DEFAULTFONT;
-        changeParaFont(OTHERFONT);
-    } else {
-        FONTTOGGLE = true;
-        document.getElementById('toggleParaFontButton').innerHTML = OTHERFONT;
-        changeParaFont(DEFAULTFONT);
-    }
+  if (FONTTOGGLE) {
+    FONTTOGGLE = false;
+    document.getElementById('toggleParaFontButton').innerHTML = DEFAULTFONT;
+    changeParaFont(OTHERFONT);
+  } else {
+    FONTTOGGLE = true;
+    document.getElementById('toggleParaFontButton').innerHTML = OTHERFONT;
+    changeParaFont(DEFAULTFONT);
+  }
 }
